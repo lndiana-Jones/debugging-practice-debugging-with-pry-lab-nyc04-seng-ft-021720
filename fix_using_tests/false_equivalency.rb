b0cdf8c70618534d97386f16e298bae208741366
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-13 05:51:16 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,9 @@ end
 
 def selection(num)
   if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
+    "HAM HAM HAM IN MY TUMMY"
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+     "YUM YUM MUNCH MUNCH MUNCH"
   end
   binding.pry
 end
