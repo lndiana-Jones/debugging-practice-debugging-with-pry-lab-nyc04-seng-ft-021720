711ba6e9543bd9c4f83b6e4a451da6f97fce178e
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-13 05:58:15 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,10 +11,10 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
     "YUM YUM MUNCH MUNCH MUNCH"
   end
-  if num = 2
+  if num == 2
     "HAM HAM HAM IN MY TUMMY"
     
   end
@@ -25,4 +25,3 @@ def runner
   selection(get_user_input)
 end
 
-p selection(1)
